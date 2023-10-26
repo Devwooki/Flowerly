a1f81112a17a54faf7b28f83e48b4aa059dd439a
김동민 <anwjr41@naver.com>
2023-10-26 22:30:48 +0900

    feat: 색상표 추가, 네비바 추가

--- Frontend/src/pages/_app.tsx
+++ Frontend/src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import Navi from "@/components/navi/Navi";
 import "../styles/globals.css";
 import "@styles/Home.module.css";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Navi />
+    </>
+  );
 }
