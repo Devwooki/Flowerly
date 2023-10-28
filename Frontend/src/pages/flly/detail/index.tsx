46bbae4ccec833e51a7dbf145720fef528e9c5d4
정현모 <wjdgusaho@naver.com>
2023-10-29 07:01:15 +0900

    feat : 판매자 Flly 페이지 , 판매자 Flly 자세히 보기페이지 디자인 구현

---
+++ Frontend/src/pages/flly/detail/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import FllySellerDetail from "@/components/flly/fllySeller/FllySellerDetail";
+
+const fllyDetail = () => {
+  return (
+    <>
+      <FllySellerDetail />
+    </>
+  );
+};
+
+export default fllyDetail;
---
+++ Frontend/src/pages/flly/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import FllySeller from "@/components/flly/fllySeller/FllySeller";
+
+const fllyMain = () => {
+  return (
+    <>
+      <FllySeller />
+    </>
+  );
+};
+
+export default fllyMain;
