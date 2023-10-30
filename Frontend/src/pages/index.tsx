eb7396fee445c7e68b7fa5c1b5ba47b974308fd0
김동민 <anwjr41@naver.com>
2023-10-30 17:55:43 +0900

    feat: 페이지 전환 애니메이션

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import style from "@styles/Home.module.css";
 import { useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
   // const router = useRouter();
@@ -45,6 +46,42 @@ export default function Home() {
   //     throw error;
   //   }
   // }
+  const [cardOrder, setCardOrder] = useState([0, 1, 2]);
+
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      src: "/img/homeBanner/164_red_phalaenopsis.jpg",
+      alt: "호접란",
+      date: "11월 ~ 3월",
+      desc: "빨간색 호접란",
+      message: "강렬한 사랑과 매력",
+    },
+    {
+      id: 2,
+      src: "/img/homeBanner/141_purple_gladiolus.jpg",
+      alt: "글라디올러스",
+      date: "5월 ~ 11월",
+      desc: "보라색 글라디올러스",
+      message: "우아함, 고귀함, 아름다움",
+    },
+    {
+      id: 3,
+      src: "/img/homeBanner/121_pink_gomphrena.jpg",
+      alt: "천일홍",
+      date: "7월 ~ 12월",
+      desc: "분홍색 천일홍",
+      message: "우아함, 달콤함, 치절, 애정",
+    },
+  ]);
+
+  const handleUpClick = () => {
+    setCardOrder([cardOrder[2], cardOrder[0], cardOrder[1]]);
+  };
+
+  const handleDownClick = () => {
+    setCardOrder([cardOrder[1], cardOrder[2], cardOrder[0]]);
+  };
 
   return (
     <div className={style.home}>
@@ -53,12 +90,28 @@ export default function Home() {
         <div className={style.title}>플리가 추천하는</div>
         <div className={style.title}>가을의 꽃을 만나보세요</div>
       </div>
-      <div className={style.bannerImg}>
-        <Image src="/img/homeBanner/cosmos.jpg" alt="코스모스" fill />
-        <div className={style.imgDisc}>
-          <div>9월~10월</div>
-          <div>핑크뮬리</div>
-          <div>꽃말쓸꺼임 그냥 낭만 문구쓸거임</div>
+
+      <div className={style.mainBody}>
+        {cardOrder.map((order, index) => {
+          const card = cards[order];
+          return (
+            <div key={card.id} className={`${style.bannerImg} ${style[`card${index + 1}`]} `}>
+              <Image src={card.src} alt={card.alt} fill />
+              <div className={style.imgDisc}>
+                <div>{card.date}</div>
+                <div>{card.desc}</div>
+                <div>{card.message}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={style.btnPakage}>
+        <div className="card-nav-btn card-nav-btn-up" onClick={handleDownClick}>
+          <span className="arrow">❮</span>
+        </div>
+        <div className="card-nav-btn card-nav-btn-down" onClick={handleUpClick}>
+          <span className="arrow">❯</span>
         </div>
       </div>
     </div>
