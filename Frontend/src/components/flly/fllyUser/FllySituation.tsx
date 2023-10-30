3171aec2ae2c70e110b8dfaf301953f8096ca3ba
김하영 <haha0691@koreatech.ac.kr>
2023-10-30 17:52:34 +0900

    feat: target, color

--- Frontend/src/components/flly/fllyUser/FllySituation.tsx
+++ Frontend/src/components/flly/fllyUser/FllySituation.tsx
@@ -9,7 +9,7 @@ const FllySeller = () => {
   const [situation, setSituation] = useRecoilState(situationState);
   const [selected, setSelected] = useState<string>("");
   const selectList = ["축하", "응원", "건강", "사랑", "감사", "사과", "위로", "선택안함"];
-  const imgList = ["/img/icon/situation01.png", "/img/icon/situation02.png", "/img/icon/situation02.png", "/img/icon/situation04.png", "/img/icon/situation05.png", "/img/icon/situation06.png", "/img/icon/situation07.png", "/img/icon/situation08.png"];
+  const imgList = ["/img/icon/situation01.png", "/img/icon/situation02.png", "/img/icon/situation03.png", "/img/icon/situation04.png", "/img/icon/situation05.png", "/img/icon/situation06.png", "/img/icon/situation07.png", "/img/icon/situation08.png"];
   const handleSelect = (e:string) => {
     setSelected(e);
     setSituation(e);
--- Frontend/src/pages/flly/index.tsx
+++ Frontend/src/pages/flly/index.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 
 import FllySeller from "@/components/flly/fllySeller/FllySeller";
 import FllySituation from "@/components/flly/fllyUser/FllySituation"
+import FllyTarget from "@/components/flly/fllyUser/FllyTarget"
+import FllyCollor from "@/components/flly/fllyUser/FllyColor"
 
 const FllyMain = () => {
   const [userType, setUserType] = useState<string>("user");
 
-  return <>{userType === "seller" && <FllySeller /> || userType !== "seller" && <FllySituation />}</>;
+  return <>{userType === "seller" && <FllySeller /> || userType !== "seller" && <FllyCollor />}</>;
 };
 
 export default FllyMain;
