6682bcaf03301e8a87a84522b74a74debf667ada
이현욱 <dldnr789@gmail.com>
2023-10-31 14:46:12 +0900

    feat. s3 업로드 구현 및 더미토큰 생성

--- Backend/src/main/java/com/ssafy/flowerly/config/S3Config.java
+++ Backend/src/main/java/com/ssafy/flowerly/config/S3Config.java
@@ -11,16 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+    private static String accessKey;
 
     @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+    private static  String secretKey;
 
     @Value("${cloud.aws.region.static}")
-    private String region;
+    private static String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public static AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
