b3b2384c031840205f750b75af4a3e4d61aa0ef6
김하영 <haha0691@koreatech.ac.kr>
2023-10-31 10:50:39 +0900

    feat: select flowers

--- Frontend/src/pages/flly/index.tsx
+++ Frontend/src/pages/flly/index.tsx
@@ -4,11 +4,12 @@ import FllySeller from "@/components/flly/fllySeller/FllySeller";
 import FllySituation from "@/components/flly/fllyUser/FllySituation"
 import FllyTarget from "@/components/flly/fllyUser/FllyTarget"
 import FllyCollor from "@/components/flly/fllyUser/FllyColor"
+import FllyFlower from "@/components/flly/fllyUser/FllyFlower"
 
 const FllyMain = () => {
   const [userType, setUserType] = useState<string>("user");
 
-  return <>{userType === "seller" && <FllySeller /> || userType !== "seller" && <FllyCollor />}</>;
+  return <>{userType === "seller" && <FllySeller /> || userType !== "seller" && <FllyFlower />}</>;
 };
 
 export default FllyMain;
