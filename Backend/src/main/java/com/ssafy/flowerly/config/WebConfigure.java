3a10ff67e319314404319489a4b95c4b7b02c468
이현욱 <dldnr789@gmail.com>
2023-11-01 09:32:05 +0900

    fix. cors url

--- Backend/src/main/java/com/ssafy/flowerly/config/WebConfigure.java
+++ Backend/src/main/java/com/ssafy/flowerly/config/WebConfigure.java
@@ -9,15 +9,10 @@ import org.springframework.web.servlet.mvc.WebContentInterceptor;
 @Configuration
 public class WebConfigure implements WebMvcConfigurer {
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new WebContentInterceptor())
-                .addPathPatterns("/**");
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("X-Custom-Header", "Content-Type", "Authorization")
                 .exposedHeaders("Authorization")
