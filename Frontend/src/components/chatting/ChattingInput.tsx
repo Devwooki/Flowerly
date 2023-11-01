3a46852e2d7fa66261c24adbb556dc0c2bc39c94
정수정 <worend365@naver.com>
2023-11-01 13:32:45 +0900

    feat: 메세지 전송 화면 구현

--- Frontend/src/components/chatting/ChattingInput.tsx
+++ Frontend/src/components/chatting/ChattingInput.tsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
 import style from "./style/ChattingInput.module.css";
+import Image from "next/image";
 
-const ChattingInput = () => {
+type ChattingInputProps = {
+  sendHandler: Function;
+};
+
+const ChattingInput: React.FC<ChattingInputProps> = ({ sendHandler }) => {
+  const [sendMsg, setSendMsg] = useState("");
+  const sendMessage = () => {
+    sendHandler(sendMsg);
+    setSendMsg("");
+  };
+
   return (
     <>
-      <div className={style.inputMain}>인풋창</div>
+      <div className={style.inputMain}>
+        <div className={style.left}>
+          <Image src="/img/icon/chatting-plus.png" width={22} height={22} alt="채팅 더보기 버튼" />
+        </div>
+        <div className={style.right}>
+          <input
+            type="text"
+            className={style.messageInput}
+            value={sendMsg}
+            onChange={(e) => {
+              setSendMsg(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") sendMessage();
+            }}
+          ></input>
+          <div className={style.sendBtn}>
+            <Image
+              src="/img/icon/chatting-send.png"
+              width={22}
+              height={22}
+              alt="메세지 전송 버튼"
+              onClick={sendMessage}
+            />
+          </div>
+        </div>
+        {/* <div className={style.right}></div> */}
+      </div>
     </>
   );
 };
