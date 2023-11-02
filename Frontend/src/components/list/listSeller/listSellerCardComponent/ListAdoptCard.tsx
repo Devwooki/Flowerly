547f1659ad5c62f6c9514556c557e6b6f7018848
정현모 <wjdgusaho@naver.com>
2023-11-03 04:02:41 +0900

    feat : 판매자 리스트 디자인 변경 및 API 연동

--- Frontend/src/components/list/listSeller/listSellerCardComponent/ListAdoptCard.tsx
+++ Frontend/src/components/list/listSeller/listSellerCardComponent/ListAdoptCard.tsx
@@ -2,54 +2,84 @@ import React, { useState } from "react";
 import style from "./ListAdoptCard.module.css";
 import Image from "next/image";
 
-interface Props {
-  ModalChangeHandler: () => void;
+interface adoptType {
+  requestId: number;
+  orderName: string;
+  phoneNumber: string;
+  orderType: string;
+  deliveryPickupTime: string;
+  fllyId: number;
+  progress: string;
 }
 
-const ListAdoptCard = ({ ModalChangeHandler }: Props) => {
+interface Props {
+  ModalChangeHandler: () => void;
+  $adoptInfo: adoptType;
+  SelectIdChangeHandler: (fllyId: number, index: number) => void;
+  $index: number;
+}
+
+const ListAdoptCard = ({
+  ModalChangeHandler,
+  SelectIdChangeHandler,
+  $adoptInfo,
+  $index,
+}: Props) => {
   const urlSrc = "/img/icon/flower-bouquet.png";
+
+  const btnClickHandler = () => {
+    SelectIdChangeHandler($adoptInfo.fllyId, $index);
+    ModalChangeHandler();
+  };
 
   return (
     <>
-      <div className={style.cardBack}>
-        <div className={style.cardHeader}>
-          <div className={style.cardHeaderImg}>
-            <div style={{ backgroundImage: `url(${"/test/test-flower-img.png"})` }} />
-          </div>
-          <div className={style.cardHeaderInfoBox}>
-            <div className={style.cardHeaderInfo}>
-              <div>
-                <Image src="/img/icon/seller-user.png" width={18} height={18} alt="상태이미지" />
-                <div>김동민</div>
+      {$adoptInfo.progress !== "픽업/배달완료" ? (
+        <div className={style.cardBack}>
+          <div className={style.cardHeader}>
+            <div className={style.cardHeaderImg}>
+              <div style={{ backgroundImage: `url(${"/test/test-flower-img.png"})` }} />
+            </div>
+            <div className={style.cardHeaderInfoBox}>
+              <div className={style.cardHeaderInfo}>
+                <div>
+                  <Image src="/img/icon/seller-user.png" width={18} height={18} alt="상태이미지" />
+                  <div>{$adoptInfo.orderName}</div>
+                </div>
+                <div>
+                  <Image src="/img/icon/seller-phon.png" width={18} height={18} alt="상태이미지" />
+                  <div>{$adoptInfo.phoneNumber}</div>
+                </div>
+                <div>
+                  <Image
+                    src="/img/icon/seller-forward.png"
+                    width={18}
+                    height={18}
+                    alt="상태이미지"
+                  />
+                  <div>{$adoptInfo.orderType}</div>
+                </div>
+                <div>
+                  <Image src="/img/icon/seller-time.png" width={18} height={18} alt="상태이미지" />
+                  <div>{$adoptInfo.deliveryPickupTime}</div>
+                </div>
               </div>
               <div>
-                <Image src="/img/icon/seller-phon.png" width={18} height={18} alt="상태이미지" />
-                <div>010-1234-5678</div>
-              </div>
-              <div>
-                <Image src="/img/icon/seller-forward.png" width={18} height={18} alt="상태이미지" />
-                <div>배달</div>
-              </div>
-              <div>
-                <Image src="/img/icon/seller-time.png" width={18} height={18} alt="상태이미지" />
-                <div>23.10.21 18:00</div>
+                주문서 보기<span> &gt;</span>
               </div>
             </div>
-            <div>
-              주문서 보기<span> &gt;</span>
+          </div>
+          <div className={style.cardMain}>
+            <div className={style.cardMainState}>
+              <Image src={urlSrc} width={16} height={16} alt="상태이미지"></Image>
+              <span>{$adoptInfo.progress}</span>
+            </div>
+            <div className={style.cardFinshBtn} onClick={btnClickHandler}>
+              완료하기
             </div>
           </div>
         </div>
-        <div className={style.cardMain}>
-          <div className={style.cardMainState}>
-            <Image src={urlSrc} width={16} height={16} alt="상태이미지"></Image>
-            <span>제작중</span>
-          </div>
-          <div className={style.cardFinshBtn} onClick={ModalChangeHandler}>
-            완료하기
-          </div>
-        </div>
-      </div>
+      ) : null}
     </>
   );
 };
--- Frontend/src/components/list/listSeller/listSellerCardComponent/ListAdoptCheckModal.tsx
+++ Frontend/src/components/list/listSeller/listSellerCardComponent/ListAdoptCheckModal.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./ListAdoptCheckModal.module.css";
+import axios from "axios";
+import { ToastErrorMessage, ToastSuccessMessage } from "@/model/toastMessageJHM";
 
 interface Props {
   ModalChangeHandler: () => void;
+  $selectId: number;
+  UpdateAdptList: (fllyUpdateProgress: string) => void;
 }
 
-const ListAdoptCheckModal = ({ ModalChangeHandler }: Props) => {
+const ListAdoptCheckModal = ({ ModalChangeHandler, $selectId, UpdateAdptList }: Props) => {
   const NotClickEventHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     //상위 함수를 실행하지 말아라 (모달 꺼지는거 방지)
     e.stopPropagation();
+  };
+
+  const SummitBtnHandler = () => {
+    axios.patch("https://flower-ly.co.kr/api/seller/flly/update/" + $selectId).then((res) => {
+      const rData = res.data;
+      if (rData.code === 200) {
+        console.log(rData);
+        ToastSuccessMessage(rData.message);
+        UpdateAdptList(rData.data.fllyUpdateProgress);
+        ModalChangeHandler();
+      } else {
+        ToastErrorMessage(rData.message);
+        ModalChangeHandler();
+      }
+    });
   };
 
   return (
@@ -19,7 +38,7 @@ const ListAdoptCheckModal = ({ ModalChangeHandler }: Props) => {
           <div>주문자에게 보여지는 상태가 변경됩니다.</div>
           <div className={style.modalBtnBox}>
             <div onClick={ModalChangeHandler}>취소</div>
-            <div>확인</div>
+            <div onClick={SummitBtnHandler}>확인</div>
           </div>
         </div>
       </div>
