79c5c0fd2751723227ec58b01249d120ab24c292
정현모 <wjdgusaho@naver.com>
2023-11-02 12:56:49 +0900

    feat : 판매자 API 전체적으로 변경 및 수정 (#209)

--- Backend/src/main/java/com/ssafy/flowerly/member/model/MemberRepository.java
+++ Backend/src/main/java/com/ssafy/flowerly/member/model/MemberRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.flowerly.member.model;
 
 import com.ssafy.flowerly.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findBySocialId(String socialId);
     Optional<Member> findByMemberId(Long memberId);
     Optional<Member> findByMemberIdAndIsRemovedFalse(Long memberId);
+
+    @Query(
+            "SELECT m FROM Member m " +
+                    "Where m.role != 'DELETE' "
+    )
+    Optional<Member> findByMemberIdActivate(Long memberID);
 
 }
