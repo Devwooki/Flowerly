c1622c06c951814dee85e37abbd2a427a8aa1637
정수정 <worend365@naver.com>
2023-11-02 21:02:13 +0900

    feat: 채팅 리스트 api 연결

--- Frontend/src/components/chatting/list/ChattingList.tsx
+++ Frontend/src/components/chatting/list/ChattingList.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./ChattingList.module.css";
+
+import axios from "axios";
 
 import ChattingListCard from "./ChattingListCard";
 
@@ -12,30 +14,18 @@ type Chatting = {
 };
 
 const ChattingList = () => {
-  // axios로 받아야 함!!
-  const [chattings, setChattings] = useState<Chatting[]>([
-    {
-      chattingId: 1,
-      lastChattingTime: "2023-10-30",
-      lastChattingMessage: "예쁘게 부탁드려요~",
-      opponentMemberId: 1,
-      opponentName: "아름다운 꽃가게",
-    },
-    {
-      chattingId: 2,
-      lastChattingTime: "2023-10-29",
-      lastChattingMessage: "혹시 이렇게는 안될까요?",
-      opponentMemberId: 2,
-      opponentName: "우리동네 꽃가게",
-    },
-    {
-      chattingId: 3,
-      lastChattingTime: "2023-10-29",
-      lastChattingMessage: "감사합니다!!",
-      opponentMemberId: 3,
-      opponentName: "수정꽃집",
-    },
-  ]);
+  const [chattings, setChattings] = useState<Chatting[]>([]);
+
+  const axiosHandler = () => {
+    axios.get(`https://flower-ly.co.kr/api/chatting`).then((response) => {
+      // console.log(response.data.data);
+      setChattings(response.data.data);
+    });
+  };
+
+  useEffect(() => {
+    axiosHandler();
+  }, [chattings]);
 
   return (
     <>
