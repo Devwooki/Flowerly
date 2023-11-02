9655d392f257d655a7ab764c7681e2a0a4d6a4f6
김동민 <anwjr41@naver.com>
2023-11-02 20:44:23 +0900

    feat: 마이페이지 회원가입 라우팅

--- Frontend/src/components/list/listBuyer/fllylistComponent/ShopCard.tsx
+++ Frontend/src/components/list/listBuyer/fllylistComponent/ShopCard.tsx
@@ -10,15 +10,15 @@ const ShopCard = () => {
       </div>
       <div className={style.shopInfo}>
         <div className={style.infoTable}>
-          <div>이름</div>
+          <Image src={"/img/icon/seller-flower.png"} alt="추천 꽃다발" width={15} height={15} />
           <div>꼬까게</div>
         </div>
         <div className={style.infoTable}>
-          <div>위치</div>
+          <Image src={"/img/icon/seller-location.png"} alt="추천 꽃다발" width={10} height={15} />
           <div>대전 유성구 학하서로 11</div>
         </div>
         <div className={style.infoTable}>
-          <div>제안</div>
+          <Image src={"/img/icon/seller-money.png"} alt="추천 꽃다발" width={15} height={15} />
           <div>25,000</div>
         </div>
         <div>요청 사항 대로 예쁘게 만들어 드릴게요. 이렇게 하면 어떠신가요?</div>
