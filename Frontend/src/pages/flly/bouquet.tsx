088adac47a3d88ed72594667c79b6b016fb5b6ff
김하영 <haha0691@koreatech.ac.kr>
2023-11-03 13:39:45 +0900

    feat: bouquet recoil

--- Frontend/src/pages/flly/bouquet.tsx
+++ Frontend/src/pages/flly/bouquet.tsx
@@ -2,43 +2,80 @@
 import React, {useEffect} from "react";
 import style from "@/components/flly/fllyUser/FllyBouquet.module.css";
 import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { bouquetState, bouquetType } from "@/recoil/fllyRecoil";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { bouquetsState, bouquetState, bouquetType } from "@/recoil/fllyRecoil";
 import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import CheckModal from "@/components/flly/fllyUser/CheckModal";
 
 const FllyBouquet = () => {
-  const router = useRouter();
-  const [check, setCheck] = useState<boolean>(false);
+  const [showPrevModal, setShowPrevModal] = useState<boolean>(false);
+  const [showNextModal, setShowNextModal] = useState<boolean>(false);
+  const [showAgainModal, setShowAgainModal] = useState<boolean>(false);
   const [selected, setSelected] = useState<bouquetType>();
   const [cnt, setCnt] = useState<number>(2);
-  const bouquets = useRecoilValue(bouquetState);
+  const bouquets = useRecoilValue(bouquetsState);
+  const [bouquet, setBouquet] = useRecoilState(bouquetState);
 
   const handleSelect = (e:bouquetType) => {
     setSelected(e);
+    setBouquet(e);
   };
 
   useEffect(() => {
     setCnt(3-(bouquets.length/4));
-  }, []);
-
-  useEffect(() => {
-    if(bouquets.length != 0)setCheck(true);
-    else setCheck(false);
   }, [bouquets]);
 
-  const handleClick = () => {
-    if(check) router.push("request");
+  const handleNextClick = () => {
+    setShowNextModal(true);
   };
 
-  const handleClickAgain = () => {
-    if(cnt > 0) router.push("loading");
+  const handlePrevClick = () => {
+    setShowPrevModal(true);
+  }
+
+  const handleAgainClick = () => {
+    if(cnt > 0) setShowAgainModal(true);
   };
+
+  const prevBtnHandler = () => {
+    setShowPrevModal(!showPrevModal);
+  }
+
+  const nextBtnHandler = () => {
+    setShowNextModal(!showNextModal);
+  }
+
+  const againBtnHandler = () => {
+    setShowAgainModal(!showAgainModal);
+  }
 
   return (
     <>
       <div className={style.fllyBox}>
+        {showPrevModal && 
+          <CheckModal
+            ModalChangeHandler={prevBtnHandler}
+            question={"이전의 선택을 변경하시겠습니까?"}
+            explain={"꽃 선택으로 이동합니다."}
+            routerHref={"flower"}
+          />
+        }
+        {showNextModal &&
+          <CheckModal
+            ModalChangeHandler={nextBtnHandler}
+            question={"선택한 시안으로 의뢰하시겠습니까?"}
+            explain={"주문 의뢰서로 이동합니다."}
+            routerHref={"request"}
+          />
+        }
+        {showAgainModal && 
+          <CheckModal
+            ModalChangeHandler={againBtnHandler}
+            question={"꽃다발을 다시 생성하시겠습니까?"}
+            explain={"이전의 꽃다발 이미지는 유지됩니다."}
+            routerHref={"loading"}
+          />
+        }
         <div className={style.contentBox}>
           <div className={style.headerTitle}>
             <div className={style.guide}>원하는 시안을 선택해주세요.</div>
@@ -53,14 +90,12 @@ const FllyBouquet = () => {
                 </div>
               ))}
             </div>
-            <div onClick={handleClickAgain} className={style.againBtn}><Image src="/img/icon/again.png" width={40} height={40} alt="again"></Image></div>
+            <div onClick={handleAgainClick} className={style.againBtn}><Image src="/img/icon/again.png" width={40} height={40} alt="again"></Image></div>
             <div className={style.btnCount}>남은 생성 횟수 : {cnt}/3</div>
           </div>
           <div className={style.btnBox}>
-            <Link href="/flly/flower">
-              <div className={style.prevBtn}>&lt;</div>
-            </Link>
-            <div onClick={handleClick} className={style.nextBtn}>다음</div>
+            <div onClick={handlePrevClick} className={style.prevBtn}>&lt;</div>
+            <div onClick={handleNextClick} className={style.nextBtn}>다음</div>
           </div>
         </div>
       </div>
