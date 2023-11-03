9723c91e758379a977e68c1c1dcebf34b781ffa4
이현욱 <dldnr789@gmail.com>
2023-11-03 17:21:28 +0900

    fix

--- Backend/src/main/java/com/ssafy/flowerly/entity/FileInfo.java
+++ Backend/src/main/java/com/ssafy/flowerly/entity/FileInfo.java
@@ -1,9 +1,6 @@
 package com.ssafy.flowerly.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +10,7 @@ import javax.persistence.Lob;
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FileInfo {
     @Id
     @GeneratedValue
