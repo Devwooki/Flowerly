2a7483df2207a74d21e4a423d665f4a9e9b62938
김동민 <anwjr41@naver.com>
2023-11-05 04:14:36 +0900

    feat: 가게 정보 지도에 그리기

--- Frontend/src/pages/list/shop/[shopId].tsx
+++ Frontend/src/pages/list/shop/[shopId].tsx
@@ -1,7 +1,5 @@
-import { log } from "console";
-import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./ShopInfoMain.module.css";
 import ShopLocation from "@/components/list/listBuyer/shopInfo/ShopLocation";
 import ShopImg from "@/components/list/listBuyer/shopInfo/ShopImg";
@@ -10,11 +8,13 @@ import ShopReview from "@/components/list/listBuyer/shopInfo/ShopReview";
 const ShopInfoMain = () => {
   const router = useRouter();
 
-  const shopInfo = {
-    shopX: 123,
-    shopY: 4342,
-    shopName: "아름다운 꽃가게",
-    shopLoc: "대전광역시 유성구 학하서로 11 (대전광역시 유성구 덕명동 600-1)",
+  useEffect(() => {
+    console.log(router.query); // 전달받은 샵 ID를 Api한테 쓰면 댐
+  }, []);
+
+  const ShopInfoDetail: ShopInfoDetail = {
+    shopName: "나이테플라워",
+    shopLoc: "대전광역시 중구 대둔산로 384",
     shopImg: [
       "https://neighbrew.s3.ap-northeast-2.amazonaws.com/FlOWER/9ab5e8bb-56f9-4cff-967b-22075b73e37a010_pink_gookhwa.jpg.jpg",
       "https://neighbrew.s3.ap-northeast-2.amazonaws.com/FlOWER/da3f0e9f-0c1e-45d8-ba34-86e57331687b011_yellow_gookhwa.jpg.jpg",
@@ -28,8 +28,8 @@ const ShopInfoMain = () => {
         <div className={style.headerTitle}>가게정보</div>
       </div>
       <div className={style.ShopInfoMain}>
-        <ShopLocation shopInfo={shopInfo} />
-        {/* <ShopImg shopImg={shopInfo.shopImg} /> */}
+        <ShopLocation ShopInfoDetail={ShopInfoDetail} />
+        <ShopImg shopImg={ShopInfoDetail.shopImg} />
         <ShopReview />
       </div>
     </div>
