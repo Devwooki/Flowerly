f75e0a5b94fbff445fdaac2aed71d808264b77ee
김동민 <anwjr41@naver.com>
2023-11-06 02:28:08 +0900

    feat: 가게 클릭시 큰 모달창, 가게 상세 정보 페이지 이동

--- Frontend/src/components/list/listBuyer/listBuyerCardComponent/BuyerCards.tsx
+++ Frontend/src/components/list/listBuyer/listBuyerCardComponent/BuyerCards.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./BuyerCards.module.css";
 import ProgressBar from "./ProgressBar";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { FllylistDiscRecoil } from "@/recoil/kdmRecoil";
 
 type BuyerCardsProps = {
@@ -13,39 +13,50 @@ type BuyerCardsProps = {
 const stateProps = ["입찰", "조율", "주문완료", "제작완료", "픽업/배달완료"];
 
 const BuyerCards = ({ card }: BuyerCardsProps) => {
+  const isClient = typeof window !== "undefined";
+
   const route = useRouter();
   const stepNumber = stateProps.indexOf(card.state);
   const setCardProps = useSetRecoilState(FllylistDiscRecoil);
 
   const posFlowerLeft = (step: number) => {
     if (step === 0) {
-      return "0";
+      return "1%";
     }
     if (step === 1) {
       return "16%";
     }
     if (step === 2) {
-      return "32%";
+      return "34%";
     }
     if (step === 3) {
-      return "53%";
+      return "54%";
     }
     if (step === 4) {
-      return "86%";
+      return "87%";
     }
   };
 
-  // useEffect(() => {
-  //   const handleRouteChange = (url, { shallow }) => {
-  //     setCardProps(card);
-  //   };
-
-  //   route.events.on("routeChangeComplete", handleRouteChange);
-
-  //   return () => {
-  //     route.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [card]);
+  const mapColorNameToRGB = (colorName: string) => {
+    switch (colorName) {
+      case "빨간색":
+        return "rgb(219,68,85)";
+      case "주황색":
+        return "rgb(255,165,0)";
+      case "분홍색":
+        return "rgb(255,192,203)";
+      case "노랑색":
+        return "rgb(251,232,112)";
+      case "파랑색":
+        return "rgb(4,137,221)";
+      case "보라색":
+        return "rgb(206,146,216)";
+      case "흰색":
+        return "rgb(255,255,255)";
+      default:
+        return "black"; // 만약 매핑되지 않는 경우 원래 문자열 반환
+    }
+  };
 
   const fllistBtn = (fllyId: number) => {
     setCardProps(card);
@@ -57,14 +68,33 @@ const BuyerCards = ({ card }: BuyerCardsProps) => {
     });
   };
 
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    if (isClient) {
+      // 현재의 화면 너비 설정
+      setWindowWidth(window.innerWidth);
+
+      // 창 크기가 변경될 때마다 화면 너비 업데이트
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [isClient]);
+
   return (
     <div className={style.cardBox}>
-      <div style={{ height: "40px" }}>
+      <div style={{ height: "30px" }}>
         <Image
           src="/img/icon/currentFlower.png"
           alt="현재상태"
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           className={style.currentFlower}
           style={{ marginLeft: posFlowerLeft(stepNumber) }}
         />
@@ -84,9 +114,17 @@ const BuyerCards = ({ card }: BuyerCardsProps) => {
           </div>
           <div className={style.infoColorTable}>주요색상</div>
           {card.selectedColor.map((color, idx) => {
+            const rgbColor = mapColorNameToRGB(color);
             return (
-              <div key={idx} className={`${style.colorInfo} ${style.colorfirst}`}>
-                {color}
+              <div
+                key={idx}
+                className={`${style.colorInfo} ${style.colorfirst}`}
+                style={{
+                  color: isClient && window.innerWidth <= 426 ? rgbColor : "black",
+                  backgroundColor: isClient && window.innerWidth <= 426 ? "" : rgbColor,
+                }}
+              >
+                {window.innerWidth <= 426 ? "" : color}
               </div>
             );
           })}
