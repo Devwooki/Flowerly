bc8c26fc71f557eea3015c4dbfc2795b9c637724
정현모 <wjdgusaho@naver.com>
2023-11-05 22:01:44 +0900

    feat : 주문상세보기 페이지 화면설계

--- Frontend/src/components/flly/fllySeller/fllySellerDetailComponent/ResponseDetail.tsx
+++ Frontend/src/components/flly/fllySeller/fllySellerDetailComponent/ResponseDetail.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import style from "./ResponseDetail.module.css";
 
 const ResponseDetail = () => {
-  return <>ResponseDetail</>;
+  return (
+    <>
+      <div className={style.responseBack}>
+        <div className={style.detailMainTitle}>의뢰 내용</div>
+        <div className={style.detailMainBox}>
+          <div className={style.detailInfo}>
+            <div>제안금액</div>
+            <div>
+              28,000<span>원</span>
+            </div>
+          </div>
+          <div className={style.detailInfo}>
+            <div>설명</div>
+            <div>
+              ㅁㄴ이라ㅓㄴㅇ미ㅏ럼ㄴ이ㅏ럼ㄴ이ㅏㅓㄹ니ㅏ럼니ㅏㅇ;러미낭;ㅓ리만ㅇ;ㅓ리만얼미나언이ㅏㅓㄹㅇ
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ResponseDetail;
