3621cf5bae12c4724d55b0d9930d701720dd51a0
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-11-05 12:41:12 +0900

    fix: accesstoken in temp page

--- Frontend/src/pages/temp/index.tsx
+++ Frontend/src/pages/temp/index.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 import { memberInfoState } from "../../recoil/memberInfoRecoil";
 
 const Temp = () => {
+  const [path, setPath] = useState<string | null>(null);
+  const [host, setHost] = useState<string | null>(null);
   const router = useRouter();
   const setMemberInfo = useSetRecoilState(memberInfoState);
   const { token } = router.query as { token: string };
@@ -16,12 +18,19 @@ const Temp = () => {
     }
   }, [token]);
 
+  useEffect(() => {
+    setPath(window.location.pathname);
+    setHost(window.location.host);
+  }, []);
+
   const getMemberinfo = async (token: string) => {
     axios
       .get("https://flower-ly.co.kr/api/member", {
         withCredentials: true,
         headers: {
           Authorization: `Bearer ${token}`,
+          "X-Request-Host": host,
+          "X-Request-Path": path,
         },
       })
       .then((response) => {
@@ -37,6 +46,9 @@ const Temp = () => {
           router.replace(router.pathname);
 
           router.push("/");
+          console.log("로그인 성공");
+          console.log(response.data.data);
+          console.log(token);
         } else {
           console.error("로그인 실패: ", response.data.message);
         }
