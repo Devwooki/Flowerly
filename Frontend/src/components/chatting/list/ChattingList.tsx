45ead1d360451cf9920e8aea5b4f03c6ff265e29
정수정 <worend365@naver.com>
2023-11-06 22:00:46 +0900

    feat: 채팅 리스트 검색

--- Frontend/src/components/chatting/list/ChattingList.tsx
+++ Frontend/src/components/chatting/list/ChattingList.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import style from "./style/ChattingList.module.css";
 import axios from "axios";
 import Image from "next/image";
+import { Transition } from "framer-motion";
 
 import ChattingListCard from "./ChattingListCard";
 
@@ -23,6 +24,9 @@ const ChattingList = () => {
     });
   };
 
+  const [isActive, setIsActive] = useState(false);
+  const [searchKeyword, setSearchKeyword] = useState<string>();
+
   useEffect(() => {
     axiosHandler();
   }, []);
@@ -31,32 +35,58 @@ const ChattingList = () => {
     <>
       <div className={style.header}>
         <div className={style.headerTitle}>플리 채팅</div>
-        <div className={style.searchDiv}>
-          <Image
-            className={style.onlyIcon}
-            src="/img/btn/search-btn.png"
-            width={22}
-            height={22}
-            alt="상태이미지"
-          />
-        </div>
-        {/* <div className={style.searchDiv}>
-          <div className={style.right}>
-            <input />
+        <div
+          className={style.right}
+          style={{
+            width: isActive ? "45%" : "40px",
+          }}
+        >
+          {isActive && (
+            <input
+              type="text"
+              value={searchKeyword}
+              autoFocus
+              placeholder="이름 검색"
+              onChange={(e) => {
+                setSearchKeyword(e.target.value);
+              }}
+              onBlur={(e) => {
+                if (e.target.value.trim() === "") {
+                  setIsActive(false);
+                  setSearchKeyword(e.target.value.trim());
+                }
+              }}
+            />
+          )}
+          <div className={style.icon}>
             <Image
-              className={style.inputIcon}
               src="/img/btn/search-btn.png"
-              width={22}
-              height={22}
+              width={20}
+              height={20}
               alt="상태이미지"
+              onClick={() => {
+                if (!isActive) {
+                  setIsActive(true);
+                } else {
+                  // 검색 버튼 역할
+                }
+              }}
             />
           </div>
-        </div> */}
+        </div>
       </div>
       <div className={style.container}>
-        {chattings.map((chatting, idx) => {
-          return <ChattingListCard key={chatting.chattingId} chattingData={chatting} />;
-        })}
+        {chattings
+          .filter((chatting) => {
+            if (searchKeyword?.trim())
+              return chatting.opponentName
+                .toLowerCase()
+                .includes(searchKeyword?.trim().toLowerCase());
+            else return chatting;
+          })
+          .map((chatting, idx) => {
+            return <ChattingListCard key={chatting.chattingId} chattingData={chatting} />;
+          })}
       </div>
     </>
   );
