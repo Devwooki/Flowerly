fd10a22b16ca920d20b395fee67b509a6f14a4fb
정현모 <wjdgusaho@naver.com>
2023-11-06 17:00:00 +0900

    feat : 마이페이지 화면 (구매자/판매자)화면 설계 (#301)

---
+++ Frontend/src/components/mypage/MyPageComponent/MypageStoreImg.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+import style from "./MypageStoreImg.module.css";
+import Image from "next/image";
+
+const MypageStoreImg = () => {
+  const imgBoxRef = useRef<HTMLDivElement>(null);
+
+  //이미지 사이즈 변화
+  useEffect(() => {
+    if (imgBoxRef.current) {
+      let imgBoxWidth = imgBoxRef.current.offsetWidth - 10;
+      let imgSize = imgBoxWidth / 3;
+      const divElements = imgBoxRef.current.querySelectorAll("div");
+      divElements.forEach((div) => {
+        div.style.width = imgSize.toFixed() + "px";
+        div.style.height = imgSize.toFixed() + "px";
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <div className={style.StoreImg}>
+        <div>대표 사진</div>
+
+        <div className={style.ImgBox} ref={imgBoxRef}>
+          <div>
+            <Image src={"/test/test-flower-img.png"} fill alt="대표사진" />
+          </div>
+          <div>
+            <Image src={"/test/test-flower-img.png"} fill alt="대표사진" />
+          </div>
+          <div>
+            <Image src={"/test/test-flower-img.png"} fill alt="대표사진" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MypageStoreImg;
