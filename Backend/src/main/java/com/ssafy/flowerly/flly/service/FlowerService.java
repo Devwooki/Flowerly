9c5f61db2ca3eb23e6794ff5bfbf36436817d4a8
김하영 <haha0691@koreatech.ac.kr>
2023-11-07 12:17:51 +0900

    feat: flly request

--- Backend/src/main/java/com/ssafy/flowerly/flly/service/FlowerService.java
+++ Backend/src/main/java/com/ssafy/flowerly/flly/service/FlowerService.java
@@ -1,13 +1,19 @@
 package com.ssafy.flowerly.flly.service;
 
-import com.ssafy.flowerly.entity.Flower;
-import com.ssafy.flowerly.entity.type.ColorType;
-import com.ssafy.flowerly.entity.type.SituationType;
-import com.ssafy.flowerly.entity.type.TargetType;
+import com.ssafy.flowerly.address.repository.DongRepository;
+import com.ssafy.flowerly.address.repository.SidoRepository;
+import com.ssafy.flowerly.address.repository.SigunguRepository;
+import com.ssafy.flowerly.entity.*;
+import com.ssafy.flowerly.entity.type.*;
+import com.ssafy.flowerly.exception.CustomException;
+import com.ssafy.flowerly.exception.ErrorCode;
 import com.ssafy.flowerly.flly.dto.FllyDto;
 import com.ssafy.flowerly.flly.dto.FlowerDto;
 import com.ssafy.flowerly.flly.dto.FlowerRequestDto;
 import com.ssafy.flowerly.flly.repository.FlowerRepository;
+import com.ssafy.flowerly.seller.model.FllyDeliveryRegionRepository;
+import com.ssafy.flowerly.seller.model.FllyPickupRegionRepository;
+import com.ssafy.flowerly.seller.model.FllyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +28,12 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class FlowerService {
     private final FlowerRepository flowerRepository;
+    private final FllyRepository fllyRepository;
+    private final SidoRepository sidoRepository;
+    private final SigunguRepository sigunguRepository;
+    private final DongRepository dongRepository;
+    private final FllyPickupRegionRepository fllyPickupRegionRepository;
+    private final FllyDeliveryRegionRepository fllyDeliveryRegionRepository;
 
     public Map<String, List<FlowerDto>> getFlowerList(FlowerRequestDto flowerRequest) {
         Map<String, List<FlowerDto>> lists = new HashMap<>();
@@ -92,5 +104,82 @@ public class FlowerService {
     }
 
     public void saveFllyRequest(FllyDto fllyDto) {
+        Flly flly = new Flly();
+        flly.setSituation(fllyDto.getSituation());
+        flly.setTarget(fllyDto.getTarget());
+        if(fllyDto.getColors().size() > 0) {
+            flly.setColor1(ColorType.valueOf(fllyDto.getColors().get(0)));
+            if(fllyDto.getColors().size() > 1) {
+                flly.setColor2(ColorType.valueOf(fllyDto.getColors().get(1)));
+                if(fllyDto.getColors().size() > 2) flly.setColor3(ColorType.valueOf(fllyDto.getColors().get(2)));
+            }
+        }
+        if(fllyDto.getFlowers().size() > 0) {
+            Flower flower1 = flowerRepository.findById(fllyDto.getFlowers().get(0).getFlowerCode())
+                            .orElseThrow(() -> new CustomException(ErrorCode.FLOWER_NOT_FOUND));
+            flly.setFlower1(flower1);
+            if(fllyDto.getFlowers().size() > 1) {
+                Flower flower2 = flowerRepository.findById(fllyDto.getFlowers().get(1).getFlowerCode())
+                        .orElseThrow(() -> new CustomException(ErrorCode.FLOWER_NOT_FOUND));
+                flly.setFlower2(flower2);
+                if(fllyDto.getFlowers().size() > 2) {
+                    Flower flower3 = flowerRepository.findById(fllyDto.getFlowers().get(2).getFlowerCode())
+                            .orElseThrow(() -> new CustomException(ErrorCode.FLOWER_NOT_FOUND));
+                    flly.setFlower3(flower3);
+                }
+            }
+        }
+        flly.setOrderType(fllyDto.getOrderType());
+        flly.setDeadline(fllyDto.getDeadline().plusHours(9));
+        flly.setRequestContent(fllyDto.getRequestContent());
+        flly.setBudget(fllyDto.getBudget());
+        flly.setImageUrl(""); // 처리하기
+        flly.setCanceled(false);
+        flly.setProgress(ProgressType.START);
+
+        Flly savedFlly = fllyRepository.save(flly);
+        if(fllyDto.getOrderType().equals(OrderType.DELIVERY)) {
+            Sido sido = sidoRepository.findBySidoName(fllyDto.getDelivery().getSido())
+                            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FIND_SIDO));
+            Sigungu sigungu = sigunguRepository.findBySigunguNameAndSido(fllyDto.getDelivery().getSigungu(), sido)
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FIND_SIGUNGU));
+
+            Dong dong = dongRepository.findByDongNameAndSigungu(fllyDto.getDelivery().getDong(), sigungu)
+                    .orElseThrow(() -> new CustomException((ErrorCode.NOT_FIND_DONG)));
+
+            FllyDeliveryRegion fllyDeliveryRegion = FllyDeliveryRegion.builder()
+                    .flly(savedFlly)
+                    .sido(sido)
+                    .sigungu(sigungu)
+                    .dong(dong)
+                    .deliveryAddress(fllyDto.getDetailAddress())
+                    .build();
+
+            fllyDeliveryRegionRepository.save(fllyDeliveryRegion);
+        } else {
+            for (FllyDto.Pickup pickup : fllyDto.getPickup()) {
+                Long sidoCode = pickup.getSidoCode();
+                Long sigunguCode = pickup.getSigunguCode();
+                Long dongCode = pickup.getDongCode();
+
+                Sido pickupSido = sidoRepository.findBySidoCode(sidoCode)
+                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FIND_SIDO));
+
+                Sigungu pickupSigungu = sigunguRepository.findBySigunguCodeAndSido(sigunguCode, pickupSido)
+                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FIND_SIGUNGU));
+
+                Dong pickupDong = dongRepository.findByDongCodeAndSigungu(dongCode, pickupSigungu)
+                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FIND_DONG));
+
+                FllyPickupRegion fllyPickupRegion = FllyPickupRegion.builder()
+                        .flly(savedFlly)
+                        .sido(pickupSido)
+                        .sigungu(pickupSigungu)
+                        .dong(pickupDong)
+                        .build();
+
+                fllyPickupRegionRepository.save(fllyPickupRegion);
+            }
+        }
     }
 }
