70ed3c33e8b9f0456cbbb0e409e9e69070209e59
정현모 <wjdgusaho@naver.com>
2023-11-08 13:45:53 +0900

    feat : 마이페이지 판매자및 구매자 변환 확인 모달창 (#298)

---
+++ Frontend/src/components/mypage/MyPageSettingComponent/UserTypeChangeModal.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import style from "./UserTypeChangeModal.module.css";
+
+interface Props {
+  userChangModalHandler: () => void;
+}
+
+const UserTypeChangeModal = ({ userChangModalHandler }: Props) => {
+  const [userType, setUserType] = useState<string>();
+
+  const NotClickEventHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    //상위 함수를 실행하지 말아라 (모달 꺼지는거 방지)
+    e.stopPropagation();
+  };
+
+  //유저정보 변경 눌렀을때
+  const SummitBtnHandler = () => {
+    if (userType === "buyer") {
+      console.log("저 구매자인데 유저 정보변경을 눌렀어요!!!");
+    } else if (userType === "seller") {
+      console.log("저 판매자인데 유저 정보변경을 눌렀어요!!!");
+    }
+  };
+
+  return (
+    <>
+      <div className={style.checkBack} onClick={userChangModalHandler}>
+        {userType === "buyer" ? (
+          <div className={style.modalBack} onClick={NotClickEventHandler}>
+            <div>판매자로 전환하시겠습니까?</div>
+            <div>판매자로 전환시 사업자 등록 및 추가적인 정보를 입력하셔야합니다.</div>
+            <div className={style.modalBtnBox}>
+              <div onClick={userChangModalHandler}>취소</div>
+              <div onClick={SummitBtnHandler}>확인</div>
+            </div>
+          </div>
+        ) : (
+          <div className={style.modalBack} onClick={NotClickEventHandler}>
+            <div>구매자 전환하시겠습니까?</div>
+            <div>구매자로 전환시 등록하신 판매자 관련 및 채팅 데이터가 사라집니다.</div>
+            <div className={style.modalBtnBox}>
+              <div onClick={userChangModalHandler}>취소</div>
+              <div onClick={SummitBtnHandler}>확인</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default UserTypeChangeModal;
