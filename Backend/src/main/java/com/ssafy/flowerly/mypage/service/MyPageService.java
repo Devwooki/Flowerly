805127d2d1782268ba1e61876db28a1526bcd018
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-11-08 09:54:04 +0900

    feat: mypage api

--- Backend/src/main/java/com/ssafy/flowerly/mypage/service/MyPageService.java
+++ Backend/src/main/java/com/ssafy/flowerly/mypage/service/MyPageService.java
@@ -2,17 +2,26 @@ package com.ssafy.flowerly.mypage.service;
 
 
 import com.ssafy.flowerly.entity.Member;
+import com.ssafy.flowerly.entity.StoreImage;
+import com.ssafy.flowerly.entity.StoreInfo;
 import com.ssafy.flowerly.exception.CustomException;
 import com.ssafy.flowerly.exception.ErrorCode;
 import com.ssafy.flowerly.member.model.MemberRepository;
+import com.ssafy.flowerly.member.model.StoreInfoRepository;
+import com.ssafy.flowerly.mypage.dto.StoreMyPageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MyPageService {
 
     private final MemberRepository memberRepository;
+    private final StoreInfoRepository storeInfoRepository;
 
 
     public Object getNickName(Long memberId) {
@@ -33,8 +42,6 @@ public class MyPageService {
 
         return member.getNickName();
 
-
-
     }
 
 
@@ -50,4 +57,33 @@ public class MyPageService {
         return newNotificationStatus;
 
     }
+
+    @Transactional
+    public StoreMyPageDto getStoreNameAndImg(Long memberId) {
+
+        List<Object[]> results = storeInfoRepository.findBySellerInfo(memberId);
+
+        if (results.isEmpty()) {
+            throw new CustomException(ErrorCode.NOT_FIND_MEMBER);
+        }
+
+
+        StoreInfo storeInfo = (StoreInfo) results.get(0)[0];
+        String storeName = storeInfo.getStoreName();
+
+
+        List<String> imageUrls = results.stream()
+                .filter(objects -> objects[1] != null)
+                .map(objects -> ((StoreImage) objects[1]).getImageUrl())
+                .collect(Collectors.toList());
+
+        return StoreMyPageDto.builder()
+                .storeName(storeName)
+                .imageUrl(imageUrls)
+                .build();
+    }
+
+
+
+
 }
