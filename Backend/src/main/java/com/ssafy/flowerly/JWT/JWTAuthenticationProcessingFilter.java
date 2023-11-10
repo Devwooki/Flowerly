c1514e531b865efa55225e5e11b9ee5eb338c4c0
이현욱 <dldnr789@gmail.com>
2023-11-10 13:56:37 +0900

    fix. stomp-chat

--- Backend/src/main/java/com/ssafy/flowerly/JWT/JWTAuthenticationProcessingFilter.java
+++ Backend/src/main/java/com/ssafy/flowerly/JWT/JWTAuthenticationProcessingFilter.java
@@ -54,12 +54,18 @@ public class JWTAuthenticationProcessingFilter extends OncePerRequestFilter {
 
         //return을 하게 되면 Servlet 컨테이너를 넘어 Interceptor -> Controller로 접속하게 된다.
         //멤버 검증 로직은 인터셉터에서 수행하므로 filter를 넘어간다.
-        if(request.getRequestURI().equals("/api/member")
-                ||request.getRequestURI().startsWith("/stomp-chat")){
+        if(request.getRequestURI().equals("/api/member")){
             log.info("너냐!?");
             filterChain.doFilter(request, response);
             return;
         }
+
+        if(request.getRequestURI().startsWith("/stomp-chat")){
+            log.info("채팅연결 요청?");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         if(request.getRequestURI().startsWith("/api/member/dummy-token")) {
             filterChain.doFilter(request, response);
             return;
