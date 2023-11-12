3a48d9e2a9666733af8635ad1fb352df6984d48a
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-11-13 03:05:21 +0900

    feat: mypage data binding

--- Frontend/src/components/mypage/MyFllyListComponent/SellerFllyListCard/SellerFllyListCompletedCard.tsx
+++ Frontend/src/components/mypage/MyFllyListComponent/SellerFllyListCard/SellerFllyListCompletedCard.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import style from "./SellerFllyListCompletedCard.module.css";
 
-const SellerFllyListCompletedCard = () => {
+interface Order {
+  fllyId: number;
+  orderName: string;
+  orderType: string;
+  deliveryPickupTime: string;
+  progress: string;
+}
+
+interface SellerFllyListCompletedCardProps {
+  data: Order;
+}
+
+const SellerFllyListCompletedCard: React.FC<SellerFllyListCompletedCardProps> = ({ data }) => {
   return (
     <>
       <div className={style.cardBack}>
@@ -15,19 +27,19 @@ const SellerFllyListCompletedCard = () => {
           <div className={style.OrderInfoBox}>
             <div className={style.OrderInfoBoxHarf}>
               <div>주문자</div>
-              <div>김동민</div>
+              <div>{data.orderName}</div>
             </div>
             <div className={style.OrderInfoBoxHarf}>
               <div>주문유형</div>
-              <div>배달</div>
+              <div>{data.orderType}</div>
             </div>
             <div className={style.OrderInfoBoxAll}>
               <div>배송완료 일시</div>
-              <div>23.10.21. 18:00</div>
+              <div>{data.deliveryPickupTime}</div>
             </div>
           </div>
           <div className={style.OrderFooter}>
-            <div>배달 완료</div>
+            <div>{data.progress}</div>
           </div>
         </div>
       </div>
