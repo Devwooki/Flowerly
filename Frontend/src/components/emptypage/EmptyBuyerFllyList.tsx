59b73b205cb3eaf71acad0f8a424d630ba696ffd
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-11-14 14:23:49 +0900

    feat: empty page, temp token

---
+++ Frontend/src/components/emptypage/EmptyBuyerFllyList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EmptyReviewList = () => {
+  return (
+    <div>
+      <div className="empty">
+        <div className="empty__text">리뷰내역이 없습니다.</div>
+      </div>
+    </div>
+  );
+};
+export default EmptyReviewList;
