6574582975eabdcc96baeb6ae5784973f9bb68a3
김하영 <haha0691@koreatech.ac.kr>
2023-11-17 03:52:21 +0900

    fix: delete flly

--- Backend/src/main/java/com/ssafy/flowerly/chatting/repository/ChattingRepository.java
+++ Backend/src/main/java/com/ssafy/flowerly/chatting/repository/ChattingRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.flowerly.chatting.repository;
 
 import com.ssafy.flowerly.entity.Chatting;
+import com.ssafy.flowerly.entity.Flly;
 import com.ssafy.flowerly.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,4 +13,6 @@ public interface ChattingRepository extends JpaRepository<Chatting, Long> {
     Optional<Chatting> findByFllyParticipationFllyParticipationId(Long participationId);
     List<Chatting> findAllByConsumerAndIsRemovedConsumerFalseOrderByLastChattingTimeDesc(Member consumer);
     List<Chatting> findAllBySellerAndIsRemovedSellerFalseOrderByLastChattingTimeDesc(Member seller);
+
+    List<Chatting> findAllByFlly(Flly flly);
 }
--- Backend/src/main/java/com/ssafy/flowerly/flly/service/FllyService.java
+++ Backend/src/main/java/com/ssafy/flowerly/flly/service/FllyService.java
@@ -1,8 +1,11 @@
 package com.ssafy.flowerly.flly.service;
 
+import com.ssafy.flowerly.chatting.repository.ChattingRepository;
+import com.ssafy.flowerly.entity.Chatting;
 import com.ssafy.flowerly.entity.Flly;
 import com.ssafy.flowerly.entity.Review;
 import com.ssafy.flowerly.entity.StoreInfo;
+import com.ssafy.flowerly.entity.type.ChattingType;
 import com.ssafy.flowerly.entity.type.ProgressType;
 import com.ssafy.flowerly.exception.CustomException;
 import com.ssafy.flowerly.exception.ErrorCode;
@@ -17,6 +20,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -26,6 +30,7 @@ public class FllyService {
     private final ReviewRepository reviewRepository;
     private final MemberService memberService;
     private final FllyRepository fllyRepository;
+    private final ChattingRepository chattingRepository;
 
     public Map<String, Object> getStoreDetail(Pageable pageable, Long sellerId){
         Map<String, Object> responseData = new HashMap<>();
@@ -41,5 +46,11 @@ public class FllyService {
         Flly flly = fllyRepository.findByFllyId(fllyId)
                 .orElseThrow(() -> new CustomException(ErrorCode.FLLY_NOT_FOUND));
         flly.setProgress(ProgressType.CANCELED);
+        flly.setCanceled(true);
+
+        List<Chatting> chattingList = chattingRepository.findAllByFlly(flly);
+        for(Chatting chatting: chattingList) {
+            chatting.updateChattingStatus(ChattingType.CANCELED);
+        }
     }
 }
